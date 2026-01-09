6230538090c9c771ba470f8c8758d018a1ef492d
GuihuaZhou <guihuazhou0728@gmail.com>
2026-01-09 21:04:45 +0800

    feat: 支持设置玩家定居点囚犯逃离概率

--- ModifiedArmy/ModifiedArmy/Patch/PrisonerReleaseCampaignBehaviorPatch.cs
+++ ModifiedArmy/ModifiedArmy/Patch/PrisonerReleaseCampaignBehaviorPatch.cs
@@ -1,9 +1,12 @@
 ﻿using HarmonyLib;
+using ModifiedArmy.Utils;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Actions;
 using TaleWorlds.CampaignSystem.CampaignBehaviors;
+using TaleWorlds.Core;
 
 namespace ModifiedArmy.Patch
 {
@@ -24,10 +27,16 @@ namespace ModifiedArmy.Patch
                     (hero.PartyBelongedToAsPrisoner.Settlement.IsTown || hero.PartyBelongedToAsPrisoner.Settlement.IsCastle) &&
                     hero.PartyBelongedToAsPrisoner.Settlement.OwnerClan == Clan.PlayerClan)
                 {
+                    float escapeChance = Settings.Instance.PlayerSettlementPrisonerEscapeChance;
+                    ExplainedNumber explainedNumber = new ExplainedNumber(escapeChance, false, null);
+                    if (MBRandom.RandomFloat < explainedNumber.ResultNumber)
+                    {
+                        EndCaptivityAction.ApplyByEscape(hero, null, true);
+                    }
                     return false;
                 }
             }
-            
+
             return true;
         }
     }
