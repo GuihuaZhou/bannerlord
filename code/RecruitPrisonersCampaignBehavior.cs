f306f465d2a783611c3cf14d1f9b2fccb678704d
GuihuaZhou <guihuazhou0728@gmail.com>
2026-02-06 20:31:15 +0800

    feat：更新dnspy反编译代码

--- code/RecruitPrisonersCampaignBehavior.cs
+++ code/RecruitPrisonersCampaignBehavior.cs
@@ -9,7 +9,7 @@ namespace TaleWorlds.CampaignSystem.CampaignBehaviors
 	// Token: 0x0200043A RID: 1082
 	public class RecruitPrisonersCampaignBehavior : CampaignBehaviorBase
 	{
-		// Token: 0x060044D1 RID: 17617 RVA: 0x001521F4 File Offset: 0x001503F4
+		// Token: 0x060044D1 RID: 17617 RVA: 0x00152538 File Offset: 0x00150738
 		public override void RegisterEvents()
 		{
 			CampaignEvents.OnMainPartyPrisonerRecruitedEvent.AddNonSerializedListener(this, new Action<FlattenedTroopRoster>(this.OnMainPartyPrisonerRecruited));
@@ -17,7 +17,7 @@ namespace TaleWorlds.CampaignSystem.CampaignBehaviors
 			CampaignEvents.HourlyTickEvent.AddNonSerializedListener(this, new Action(this.HourlyTickMainParty));
 		}
 
-		// Token: 0x060044D2 RID: 17618 RVA: 0x00152248 File Offset: 0x00150448
+		// Token: 0x060044D2 RID: 17618 RVA: 0x0015258C File Offset: 0x0015078C
 		private void HourlyTickMainParty()
 		{
 			MobileParty mainParty = MobileParty.MainParty;
@@ -49,7 +49,7 @@ namespace TaleWorlds.CampaignSystem.CampaignBehaviors
 			}
 		}
 
-		// Token: 0x060044D3 RID: 17619 RVA: 0x00152334 File Offset: 0x00150534
+		// Token: 0x060044D3 RID: 17619 RVA: 0x00152678 File Offset: 0x00150878
 		private void DailyTickAIMobileParty(MobileParty mobileParty)
 		{
 			TroopRoster prisonRoster = mobileParty.PrisonRoster;
@@ -94,7 +94,7 @@ namespace TaleWorlds.CampaignSystem.CampaignBehaviors
 			}
 		}
 
-		// Token: 0x060044D4 RID: 17620 RVA: 0x001524BC File Offset: 0x001506BC
+		// Token: 0x060044D4 RID: 17620 RVA: 0x00152800 File Offset: 0x00150A00
 		private bool GenerateConformityForTroop(MobileParty mobileParty, CharacterObject troop, int hours = 1)
 		{
 			int xpAmount = Campaign.Current.Models.PrisonerRecruitmentCalculationModel.GetConformityChangePerHour(mobileParty.Party, troop).RoundedResultNumber * hours;
@@ -102,14 +102,14 @@ namespace TaleWorlds.CampaignSystem.CampaignBehaviors
 			return true;
 		}
 
-		// Token: 0x060044D5 RID: 17621 RVA: 0x00152500 File Offset: 0x00150700
+		// Token: 0x060044D5 RID: 17621 RVA: 0x00152844 File Offset: 0x00150A44
 		private void ApplyPrisonerRecruitmentEffects(MobileParty mobileParty, CharacterObject troop, int num)
 		{
 			int prisonerRecruitmentMoraleEffect = Campaign.Current.Models.PrisonerRecruitmentCalculationModel.GetPrisonerRecruitmentMoraleEffect(mobileParty.Party, troop, num);
 			mobileParty.RecentEventsMorale += (float)prisonerRecruitmentMoraleEffect;
 		}
 
-		// Token: 0x060044D6 RID: 17622 RVA: 0x0015253C File Offset: 0x0015073C
+		// Token: 0x060044D6 RID: 17622 RVA: 0x00152880 File Offset: 0x00150A80
 		private void RecruitPrisonersAi(MobileParty mobileParty, CharacterObject troop, int num, int conformityCost)
 		{
 			mobileParty.PrisonRoster.GetElementNumber(troop);
@@ -120,13 +120,13 @@ namespace TaleWorlds.CampaignSystem.CampaignBehaviors
 			this.ApplyPrisonerRecruitmentEffects(mobileParty, troop, num);
 		}
 
-		// Token: 0x060044D7 RID: 17623 RVA: 0x001525AB File Offset: 0x001507AB
+		// Token: 0x060044D7 RID: 17623 RVA: 0x001528EF File Offset: 0x00150AEF
 		private bool IsPrisonerRecruitable(MobileParty mobileParty, CharacterObject character, out int conformityNeeded)
 		{
 			return Campaign.Current.Models.PrisonerRecruitmentCalculationModel.IsPrisonerRecruitable(mobileParty.Party, character, out conformityNeeded);
 		}
 
-		// Token: 0x060044D8 RID: 17624 RVA: 0x001525CC File Offset: 0x001507CC
+		// Token: 0x060044D8 RID: 17624 RVA: 0x00152910 File Offset: 0x00150B10
 		private void OnMainPartyPrisonerRecruited(FlattenedTroopRoster flattenedTroopRosters)
 		{
 			foreach (CharacterObject characterObject in flattenedTroopRosters.Troops)
@@ -136,7 +136,7 @@ namespace TaleWorlds.CampaignSystem.CampaignBehaviors
 			}
 		}
 
-		// Token: 0x060044D9 RID: 17625 RVA: 0x0015262C File Offset: 0x0015082C
+		// Token: 0x060044D9 RID: 17625 RVA: 0x00152970 File Offset: 0x00150B70
 		public override void SyncData(IDataStore dataStore)
 		{
 		}
