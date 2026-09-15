8e60cccf897f8cef460db2f223fa4712e19c1957
GuihuaZhou <guihuazhou0728@gmail.com>
2026-09-15 22:56:52 +0800

    feat: 成功展示战争潜力

---
+++ ModifiedArmy/ModifiedArmy/UI/KingdomClan/ClanScrollableDetailsPrefabExtension.cs
@@ -0,0 +1,38 @@
+using Bannerlord.UIExtenderEx.Attributes;
+using Bannerlord.UIExtenderEx.Prefabs;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+
+namespace ModifiedArmy.UI.KingdomClan
+{
+    [PrefabExtension(
+        "ClansPanel",
+        "descendant::ListPanel[@IsVisible='@IsAcceptableItemSelected' and @StackLayout.LayoutMethod='VerticalTopToBottom' and Children/MaskedTextureWidget[@Brush='Kingdom.TornBanner.Big']]")]
+    public class ClanScrollableDetailsPrefabExtension
+        : PrefabExtensionReplacePatch
+    {
+        public override string Id =>
+            "ModifiedArmy_ClanScrollableDetails";
+
+        public override XmlDocument GetPrefabExtension()
+        {
+            string assemblyDirectory =
+                Path.GetDirectoryName(
+                    Assembly.GetExecutingAssembly().Location);
+
+            string xmlPath =
+                Path.Combine(
+                    assemblyDirectory,
+                    "UI",
+                    "KingdomClan",
+                    "ClanScrollableDetails.xml");
+
+            XmlDocument document = new XmlDocument();
+
+            document.Load(xmlPath);
+
+            return document;
+        }
+    }
+}
